06b3c5756ce29bdb0265629e19c399680d3b4e6c
ValEnd <92552094+Val-End@users.noreply.github.com>
2024-08-18 19:36:52 -0300

    URL Redirect fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
 import { defineMiddleware } from "astro:middleware";
 const clientID = await import.meta.env.CLIENT_ID;
+const baseURL = await import.meta.env.BASE_URL;
 
 export const onRequest = defineMiddleware((ctx, nxt) => {
    if(ctx.url.pathname === "/redirect"){
       return Response.redirect("https://id.twitch.tv/oauth2/authorize?" 
          + "response_type=code" 
          + "&client_id=" + clientID
-         + "&redirect_uri=" + "http://localhost:4321/info"
+         + "&redirect_uri=" + baseURL + "info"
          + "&scope=bits%3Aread" 
          + "&state=" + ctx.url.searchParams.get("state"));
    }
